77399b413e64eea01f6980e147b429a9c39711e7
Ahemadalikovadiya <ahemadalikovadiya4@gmail.com>
2024-12-17 21:19:15 +0530

    file update

--- Assest/Js/java.js
+++ Assest/Js/java.js
@@ -10,11 +10,18 @@ const spanPassword = document.getElementById("spanPassword");
 const addressInput = document.getElementById("addressInput");
 const addSpan = document.getElementById("addSpan");
 
+const sportInput = document.querySelectorAll('input[name="sportPlay"]');
+const spanSport = document.getElementById("spanSport");
+
 const selcetInput = document.getElementById("selcetInput");
 const spanSelect = document.getElementById("spanSelect");
 
+const tableDetailes = document.getElementById("tableDetailes");
 
-function sumbitData() {
+
+
+
+function mainData() {
     // Name
 
     if (!nameInput.value) {
@@ -57,6 +64,20 @@ function sumbitData() {
         console.log("it's Done");
     }
 
+    // Sport 
+    const sportChecked = [];
+    for (let i = 0; i < sportInput.length; i++){
+        if (sportInput[i].checked){
+            sportChecked.push(true);
+        }
+    }if(sportChecked.length == 0){
+        spanSport.style.display = "block";
+        console.log("Please Enter Your value");
+    }else{
+        spanSport.style.display = "none";
+        console.log("it's Done");
+    }
+
     // country 
     if (!selcetInput.value) {
         spanSelect.style.display = "block";
@@ -72,14 +93,68 @@ function sumbitData() {
         passwordInput.value.length > 4 &&
         passwordInput.value.length < 12
     ){
-        var formData = {
-            name : nameInput,
-            email : emailInput,
+        const formData = {
+            name : nameInput.value,
+            email : emailInput.value,
             password : passwordInput.value,
-            address : addressInput,
-            Country : selcetInput
+            address : addressInput.value,
+            Country : selcetInput.value
         };
+
+        tableData(formData);
        
     }
 }
 
+// function tableData(formData){
+//     const tr = document.createElement("tr");
+//     console.log(tr);
+//     const nameTd = document.createElement("td");
+//     const emailTd = document.createElement("td");
+//     const addressTd = document.createElement("td");
+//     const passwordTd = document.createElement("td");
+//     nameTd.innerHTML = formData.name;
+//     emailTd.innerHTML = formData.email;
+//     passwordTd.innerHTML = formData.password;
+//     addressTd.innerHTML = formData.address;
+
+//     tr.append(
+//         nameTd,
+//         emailTd,
+//         passwordTd,
+//         addressTd,
+//       );
+//       console.log(tr);
+//       tableDetailes.appendChild(tr);
+// }
+
+function tableData(formData) {
+    const tr = document.createElement("tr");
+    console.log(tr);
+    const nameTd = document.createElement("td");
+    const emailTd = document.createElement("td");
+    const addressTd = document.createElement("td");
+    const passwordTd = document.createElement("td");
+    nameTd.innerHTML = formData.name;
+    emailTd.innerHTML = formData.email;
+    addressTd.innerHTML = formData.address;
+    passwordTd.innerHTML = formData.password;
+    tr.append(
+      nameTd,
+      emailTd,
+      passwordTd,
+      addressTd,
+    );
+    console.log(tr);
+    tableDetailes.appendChild(tr);
+  }
+  function resetForm() {
+    nameInput.value = "";
+    emailInput.value = "";
+    passwordInput.value = "";
+    addressInput.value = "";
+  }
+  
+  
+  
+  
